a44580d65ec237d84f1eadce879759f1f9d527f4
timidola <70209824+timidola@users.noreply.github.com>
2020-09-07 15:35:58 +0100

    Create OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,17 @@
+x=(2,4,6,8,10)
+y=(1,3,5,7,9)
+number=int(input('enter a number from 1-10 '))
+even=(2,4,6,8,10)
+odd=(1,3,5,7,9)
+if number==x[0]:
+    print ('even number')
+elif number==even[1]:
+    print ('even number')
+elif number==even[2]:
+    print ('even number')
+elif number==even[3]:
+    print ('even number')
+elif number==even[4]:
+    print ('even number')
+else:
+    print ('odd number')
